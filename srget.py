74f3f8531467a9f3376bb8739f0b297f0609dc79
Dev Singh <dev.thep@hotmail.com>
2016-01-21 21:10:26 +0700

    Second Commit

--- srget.py
+++ srget.py
@@ -1,7 +1,7 @@
 import socket as sk 
 
 def mkDownloadRequest(serv, objName):
-	return ("GET {o} HTTP/1.1\r\n"+"Host: {s}" +"\r\n\r\n").format(o=objName, s=serv)
+	return ("GET {o} HTTP/1.1\r\n"+"Host: {s}\r\n\r\n").format(o=objName, s=serv)
 
 def mkHeadRequest(serv,objName):
 	return ("HEAD {o} HTTP/1.1\r\n"+"Host: {s}" +"\r\n\r\n").format(o=objName, s=serv)
@@ -12,14 +12,15 @@ def writeHeader(serv,objName,header_file,port):
 	sock.connect((serv,port))
 	requestHead = mkHeadRequest(serv,objName)
 	sock.send(requestHead)
+	data = ""
 	while True:
-		data = sock.recv(1024)
-		print data,
-		if len(data)==0:
+		data += sock.recv(1024)
+		#print data[len(data)-4:len(data)] == "\r\n\r\n"
+		if data[len(data)-4:len(data)] == "\r\n\r\n" or len(data)==0:
 			sock.close()
-			print "retrieved header"
+			Head.write(data)
 			break
-		Head.write(data)
+	Head.close()
 
 def writeFile(serv,objName,file,port):
 	f = open(file,"w+") # w+ both read and write
@@ -29,10 +30,12 @@ def writeFile(serv,objName,file,port):
 	sock.send(requestFile)
 	while True:
 		data = sock.recv(1024)
+		print data,
 		if len(data)==0:
 			sock.close()
 			break
 		f.write(data)
+		f.close()
 
 def NoHeaderFile(file,header_file):
 	f = open(file,"r")
@@ -43,7 +46,6 @@ def NoHeaderFile(file,header_file):
 	print header_count
 	count = 0
 	for line in lines:
-		print count
 		if count < header_count:
 			count += 1
 		else:
@@ -60,15 +62,16 @@ def getHeader(filename):
 			continue
 		elif line != "\r\n":
 			separate = line.split(":",1)
-			dct[separate[0]] = separate[1][1:len(separate)-4]
+			dct[separate[0]] = separate[1][1:len(separate[1])-2]
 		count += 1
+	f_open.close()
 	return count, dct
 
-website = "http://www.dslreports.com/forums/21"
-servName ="dslreports.com"
-objName = "/forums/21"
+website = "http://www.mahidol.ac.th/en/"
+servName ="www.mahidol.ac.th"
+objName = "/en/"
 port = 80
-#writeHeader(servName,objName,"header.txt",port)
-#print getHeader("header.txt")[0]
+writeHeader(servName,objName,"header.txt",port)
+#print getHeader("header.txt")[1]
 #writeFile(servName,objName,"test.txt",port)
-NoHeaderFile("test.txt","header.txt")
+#NoHeaderFile("test.txt","header.txt")
