64193f2d910c30122ae9cbb55d5079b6b2dfe062
Dev Singh <dev.thep@hotmail.com>
2016-01-22 15:45:46 +0700

    Fourth Commit

--- srget.py
+++ srget.py
@@ -1,5 +1,6 @@
 import socket as sk 
 import os
+from urlparse import urlparse
 
 def mkDownloadRequest(serv, objName):
 	return ("GET {o} HTTP/1.1\r\n"+"Host: {s}\r\n\r\n").format(o=objName, s=serv)
@@ -7,49 +8,62 @@ def mkDownloadRequest(serv, objName):
 def mkHeadRequest(serv,objName):
 	return ("HEAD {o} HTTP/1.1\r\n"+"Host: {s}" +"\r\n\r\n").format(o=objName, s=serv)
 
-def mkResumableRequest(serv,objName,filesize,C_len):
-	S = os.stat("header.txt")
+def mkResumableRequest(serv,objName,file):
+	S = os.stat(file)
 	size = S.st_size
-	return ("GET {o} HTTP/1.1\r\n"+"Host: {s}\r\n"+"Range: bytes={h}-"+"\r\n\r\n").format(o=objName,s=serv,a=filesize,h=str(size))
+	return ("GET {o} HTTP/1.1\r\n"+"Host: {s}\r\n"+"Range: bytes={h}-"+"\r\n\r\n").format(o=objName,s=serv,h=str(size))
 
-def writeHeader(serv,objName,header_file,port):
+def writeHeader(serv,objName,header_file,port,timeout):
 	Head = open(header_file,"w+") # w+ both read and write
 	sock = sk.socket(sk.AF_INET,sk.SOCK_STREAM)
-	sock.connect((serv,port))
-	requestHead = mkHeadRequest(serv,objName)
-	sock.send(requestHead)
-	data = ""
-	while True:
-		data += sock.recv(1024)
-		#print data[len(data)-4:len(data)] == "\r\n\r\n"
-		if data[len(data)-4:len(data)] == "\r\n\r\n" or len(data)==0:
-			sock.close()
-			Head.write(data)
-			break
+	sock.settimeout(timeout)
+	try:
+		sock.connect((serv,port))
+		requestHead = mkHeadRequest(serv,objName)
+		sock.send(requestHead)
+		data = ""
+		while True:
+			data += sock.recv(1024)
+			#print data[len(data)-4:len(data)] == "\r\n\r\n"
+			if data[len(data)-4:len(data)] == "\r\n\r\n" or len(data)==0:
+				sock.close()
+				Head.write(data)
+				break
+	except sk.gaierror, e:
+		print "Invalid URL name"
+	except sk.error, ex:
+		print "Caught exception socket.error : %s" % ex
 	Head.close()
 
-def writeFile(serv,objName,file,port):
+def writeFile(serv,objName,file,port,header_file,timeout):
+	size_header = os.stat(header_file).st_size
+	content_len = getHeader(header_file)[1]["Content-Length"]
+	print size_header , content_len
 	f = open(file,"w+") # w+ both read and write
 	sock = sk.socket(sk.AF_INET,sk.SOCK_STREAM)
+	sock.settimeout(timeout)
 	try:
 		sock.connect((serv,port))
 		requestFile = mkDownloadRequest(serv,objName)
 		#requestFile = mkResumableRequest(serv,objName,'20000','30997')
 		sock.send(requestFile)
+		length = 0
 		while True:
 			data = sock.recv(1024)
-			#print data,
-			if len(data)==0:
+			length += len(data)
+			if (length-size_header)==int(content_len) or (length-size_header)==content_len:
 				sock.close()
 				break
 			f.write(data)
 	except sk.gaierror, e:
 		print "Error Connecting to Server"
+	except sk.error, ex:
+		print "Caught exception socket.error : %s" % ex
 	f.close()
 	#print "File closed"
 	
 
-def NoHeaderFile(file,header_file):
+def NoHeaderFile(file,header_file): #split the main file from the header 
 	f = open(file,"r")
 	lines = f.readlines()
 	f.close()
@@ -64,7 +78,7 @@ def NoHeaderFile(file,header_file):
 			f.write(line)
 	f.close()
 
-def getHeader(filename):
+def getHeader(filename): #Get the header dictionary	#returns tuple (line_count, dict)
 	f_open = open(filename,"r")
 	dct = dict()
 	count = 0
@@ -79,17 +93,31 @@ def getHeader(filename):
 	f_open.close()
 	return count, dct
 
-def isDL_complete(C_len,file):
+def isDL_complete(header_File,file): #check if download is complete
+	C_len = getHeader(header_File)[0]
 	info = os.stat(file)
-	print C_len, info.st_size
 	return info.st_size == int(C_len)
 
-website = "http://www.mahidol.ac.th/en/"
-servName ="www.mahidol.ac.th"
-objName = "/en/"
+website = "http://cs.muic.mahidol.ac.th/~ktangwon/bigfile.xyz"
+pic = "http://classroomclipart.com/images/gallery/Clipart/Animals/Lion_Clipart/TN_lion-clipart-115.jpg"
+servName ="intranet.mahidol"
+objName = "/"
 port = 80
-writeHeader(servName,objName,"header.txt",port)
+timeout = 6
+#writeHeader(servName,objName,"header.txt",port,timeout)
 #print getHeader("header.txt")[1]
-writeFile(servName,objName,"test.txt",port)
+writeFile(servName,objName,"test.txt",port,"header.txt")
 #NoHeaderFile("test.txt","header.txt")
 #print isDL_complete(getHeader("header.txt")[1]["Content-Length"],"test.txt")
+def main(filename,url):
+	temp_filename = "DL" + filename
+	parsed = urlparse(url)
+	Host_serv = parsed.netloc
+	Host_path = parsed.path
+	Host_port = parsed.port
+	if Host_port == None:
+		Host_port = 80
+
+
+#print main("test.txt",website)
+
